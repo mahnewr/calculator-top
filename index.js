d622627f2f4963f8e3659f62c74fbb2d660ba233
Mahnoor Ismail <mahnoorhamidismail@gmail.com>
2024-03-04 01:29:39 +0500

    changing sign and % functionality

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 // VARIABLES
-let input1;
-let input2;
-let inpOperator;
-let operatorClicked = false;
-let secondNumber = false;
 
+// Number before operator:
+let input1;
+// Number after operator:
+let input2;
+// Operator
+let inpOperator;
+// To keep track of whether an operator has been clicked to calculate:
+let operatorClicked = false;
+// To keep track of whether we are updating input1 and input2:
+let secondNumber = false;
+// Calculator panel display:
 let displayContent = document.getElementById("display-content");
+
+// Buttons:
 const clearContent = document.getElementById("clear");
 const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
 const equate = document.getElementById("equal");
+const changeSign = document.getElementById("change-sign");
+const toPercentage = document.getElementById("percentage");
 
 // FUNCTIONS
 
@@ -45,16 +55,20 @@ const division = function (num1, num2) {
   }
 };
 
+// Function to round a non-integer to three decimal places:
 const roundToThreeDecimalPlaces = function (num) {
   const roundedNum = Math.round(num * 1000) / 1000;
   const decimalPlaces = roundedNum.toString().split(".")[1];
+
+  // Return all decimal places if they're less than 3:
   if (decimalPlaces && decimalPlaces.length < 3) {
     return roundedNum.toFixed(decimalPlaces.length);
   }
+  // Otherwise return rounded to 3 decimal places:
   return roundedNum.toFixed(3);
 };
 
-// Operate:
+// Operate input1 and input2 based on the selected operator:
 const operate = function (num1, num2, operator) {
   let result;
   if (operator === "+") {
@@ -66,7 +80,9 @@ const operate = function (num1, num2, operator) {
   } else if (operator === "÷") {
     result = division(num1, num2);
   }
-  if (Number.isInteger(result)) {
+
+  // Checking if result is an integer, otherwise rounding to 3 decimal places using function above:
+  if (Number.isInteger(result) || result === "ERROR") {
     return result;
   } else {
     return roundToThreeDecimalPlaces(result);
@@ -77,7 +93,7 @@ const operate = function (num1, num2, operator) {
 
 // Clear display content:
 clearContent.addEventListener("click", function () {
-  displayContent.innerHTML = "";
+  displayContent.innerHTML = "0";
   input1 = undefined;
   input2 = undefined;
   inpOperator = "";
@@ -85,34 +101,49 @@ clearContent.addEventListener("click", function () {
   secondNumber = false;
 });
 
-// Track number input:
+// Number input:
 numbers.forEach(function (number) {
   number.addEventListener("click", function () {
-    if (!operatorClicked) {
-      displayContent.textContent += number.textContent;
+    // Ignore '0' button input in the calculator's default state:
+    if (number.textContent === "0" && displayContent.textContent === "0") {
+      return;
     } else {
-      displayContent.textContent = number.textContent;
-      operatorClicked = false;
-    }
-    if (secondNumber) {
-      input2 = displayContent.textContent;
-    } else {
-      input1 = displayContent.textContent;
+      // If operator is not clicked and display does not show '0', number input is appended:
+      if (!operatorClicked && displayContent.textContent !== "0") {
+        displayContent.textContent += number.textContent;
+
+        // If operator is clicked or display shows '0', number input overrides existing displayed content:
+      } else {
+        displayContent.textContent = number.textContent;
+        operatorClicked = false;
+      }
+
+      // If secondNumber is true, then input2 is updated:
+      if (secondNumber) {
+        input2 = displayContent.textContent;
+      } else {
+        input1 = displayContent.textContent;
+      }
     }
   });
 });
 
-// Track operator input:
+// Operator input:
 operators.forEach(function (operator) {
   operator.addEventListener("click", function () {
+    // Executes for all operators except '=' as it has a separate function:
     if (operator.textContent !== "=") {
+      // Not being on the second number and the display not being empty means we have input1:
       if (!secondNumber && displayContent.textContent !== "") {
         operatorClicked = true;
         secondNumber = true;
         inpOperator = operator.textContent;
+
+        // If it's a subsequent operator clicked (after input2):
       } else {
-        // If it's a subsequent operator clicked
+        // Calculating the answer to input1 and input2 with previous operator when next operator is clicked:
         if (input1 !== undefined && input2 !== undefined) {
+          // input1 is replaced with the solution to the result of input1 and input2 with previous operator:
           input1 = operate(input1, input2, inpOperator);
           displayContent.textContent = input1;
           inpOperator = operator.textContent;
@@ -125,16 +156,45 @@ operators.forEach(function (operator) {
   });
 });
 
-// Track equal to button:
+// Equal to button:
 equate.addEventListener("click", function () {
+  // Only calculate solution if valid input1, input2, and operator exist:
   if (input1 !== undefined && input2 !== undefined && inpOperator !== "") {
+    // input1 is updated with result of input1 and input2 with previous operator for further calculations:
     input1 = operate(input1, input2, inpOperator);
-
     displayContent.textContent = input1;
 
+    // Reset:
     input2 = undefined;
     inpOperator = "";
     operatorClicked = false;
     secondNumber = false;
   }
+});
+
+// Change number sign:
+changeSign.addEventListener("click", function () {
+  // Check which input has to be updated:
+  if (!secondNumber) {
+    input1 = -input1;
+  } else {
+    input2 = -input2;
+  }
+
+  // Update display:
+  let currentValue = parseFloat(displayContent.textContent);
+  displayContent.textContent = -currentValue;
+});
+
+toPercentage.addEventListener("click", function () {
+  // Check which input has to be updated:
+  if (!secondNumber) {
+    input1 = input1 / 100;
+  } else {
+    input2 = input2 / 100;
+  }
+
+  // Update display:
+  let currentValue = parseFloat(displayContent.textContent);
+  displayContent.textContent = currentValue / 100;
 });
